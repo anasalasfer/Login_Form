80637f0ba45b1c52095ab5bef7e02bdd7fdc86c2
anasalasfer <142147653+anasalasfer@users.noreply.github.com>
2023-10-05 14:07:59 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+// اتصال بقاعدة البيانات (قم بتعديل المعلومات بما يتناسب مع قاعدة البيانات الخاصة بك)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_accounts";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// التحقق من اتصال قاعدة البيانات
+if ($conn->connect_error) {
+    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
+}
+
+// استقبال معلومات تسجيل الدخول من المستخدم
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+// استعلام لاستعراض المستخدم
+$sql = "SELECT * FROM users WHERE email='$email'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) == 1) {
+    $user = mysqli_fetch_assoc($result);
+    if (password_verify($password, $user["password"])) {
+        echo "<h1>HELLO</h1>";
+    } else {
+        echo "كلمة المرور غير صحيحة.";
+    }
+} else {
+    echo "المستخدم غير موجود.";
+}
+
+mysqli_close($conn);
+?>
+
---
+++ signup.php
@@ -0,0 +1,42 @@
+<?php
+// اتصال بقاعدة البيانات (قم بتعديل المعلومات بما يتناسب مع قاعدة البيانات الخاصة بك)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_accounts";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// التحقق من اتصال قاعدة البيانات
+if ($conn->connect_error) {
+    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
+}
+
+// استقبال معلومات التسجيل من المستخدم
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"]; // لا تقم بتجزئة كلمة المرور هنا
+
+// قم بتحقق من صحة المعلومات المُدخلة وتنفيذ العملية إذا تم التحقق
+if (!empty($username) && !empty($email) && !empty($password)) {
+    // هنا يمكنك إجراء المزيد من التحقق من البيانات إذا لزم الأمر
+
+    // قم بعمل تجزئة لكلمة المرور
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // قم بإعداد استعلام SQL لإدراج بيانات المستخدم الجديد
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+
+    // قم بتنفيذ الاستعلام وفحص نجاحه
+    if (mysqli_query($conn, $sql)) {
+        echo "<h1>HELLO</h1>";
+    } else {
+        echo "خطأ: " . $sql . "<br>" . mysqli_error($conn);
+    }
+} else {
+    echo "يرجى ملء جميع الحقول المطلوبة.";
+}
+
+// أغلق اتصال قاعدة البيانات
+mysqli_close($conn);
+?>
